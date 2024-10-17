9c9dd173173786dfadd074ad39271ff75bcc7253
RandyRagnar <randytsiory@gmail.com>
2024-10-17 11:38:51 +0300

    changes roles

--- app/View/Components/checkbox.php
+++ app/View/Components/checkbox.php
@@ -15,7 +15,7 @@ class checkbox extends Component
         public string $label = 'Se souvenir de moi',
         public string $type = 'checkbox', 
         public ?string $name = null,
-        public ?string $class = "form-check-input",
+        public ?string $class = null,
         public ?string $id = null
     )
     {

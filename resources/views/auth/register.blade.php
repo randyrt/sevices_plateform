ba1f3a86bb6918c3ac45102df7780f59ead734cb
RandyRagnar <randytsiory@gmail.com>
2024-10-17 12:39:11 +0300

    commit you

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
           <x-input label="E-mail"  id="email" type="email" name="email"/>
           <x-input label="Mot de passe" id="passwordInput1" type="password" name="password"/>
           <x-input label="Confirmer le mot de passe" id="passwordInput2" type="password" name="password_confirmation"/>
-          <x-checkbox  name="remember" class="form-checkbo" label="montrer/Cacher mot de passe" />
+          <x-checkbox  name="remember" class="form-checkbox" label="montrer/Cacher mot de passe" />
           <button type="submit" class="w-20 btn btn-primary">S'inscrire</button>
           <p class="mt-3">Déjà un compte, <a href="{{ route('login') }}" class="text-primary">Se connecter</a> </p>
         </form>
--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,7 +1,7 @@
+<!-- resources/views/components/checkbox.blade.php -->
 <div class="mb-2 form-check">
   <input class="{{ $class }}" id="{{ $id }}" type="{{ $type }}" name="{{ $name }}">
-  <label class="form-check-label" for="flexCheckDefault">
+  <label class="form-check-label" for="{{ $id }}">
    {{ $label }}
   </label>
 </div>
-
